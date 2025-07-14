f6baf714ee1f72e29e5c525a578e7d1e7726b11c
EsnemyC <120020955+Enysmen@users.noreply.github.com>
2025-07-14 12:43:40 +0300

    Add project files.

---
+++ Models/Shortage.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VismaTask.Models
+{
+    public class Shortage
+    {
+        // Uniquely identifies the application along with the Room
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        // Name of the user who created the request
+        public string Name { get; set; }
+
+        [Required]
+        // Room: one of the fixed values
+        public Room Room { get; set; }
+
+        [Required]
+        // Category: one of the fixed values
+        public Category Category { get; set; }
+
+        [Required]
+        // Priority from 1 (low) to 10 (high)
+        public int Priority { get; set; }
+
+
+        // Time of request creation, filled in automatically
+        public DateTime CreatedOn { get; set; }
+    }
+
+
+    // Possible rooms for application
+    public enum Room
+    {
+        MeetingRoom,
+        Kitchen,
+        Bathroom
+    }
+
+    // Possible categories for the application
+    public enum Category
+    {
+        Electronics,
+        Food,
+        Other
+    }
+}
+
---
+++ Repositories/IShortageRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using VismaTask.Models;
+
+namespace VismaTask.Repositories
+{
+    // Interface for the data storage layer
+    public interface IShortageRepository
+    {
+        // Loads all requests from storage
+        List<Shortage> LoadAll();
+
+        // Saves the full list of requests to storage
+        void SaveAll(List<Shortage> items);
+    }
+}
